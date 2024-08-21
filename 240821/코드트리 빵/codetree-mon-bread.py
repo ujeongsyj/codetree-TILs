95bedbf83d490fd9a55f4298734a1e8540f9653b
hoongel1004 <75716280+hoongel1004@users.noreply.github.com>
2024-08-21 18:23:00 +0900

    Title: 코드트리 빵, Time: 142ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 240821/코드트리 빵/codetree-mon-bread.py
@@ -0,0 +1,163 @@
+from collections import deque
+
+'''
+s1, s2, s3: 편의점
+p1, p2, p3: 사람좌표
+time
+while True:
+    if s1==p1 and s2==p2 and s3==p3:
+        break
+    
+    #1.사람 최단거리 이동
+    #2.편의점 도착
+    #3.베이스캠프로 들어가기
+'''
+
+
+N, M = map(int, input().split())
+arr = [[0]*(N+1) for _ in range(N+1)]
+for i in range(1,N+1):
+    arr[i][1:] = map(int,input().split())
+
+p = [(0, 0) for _ in range(M+1)]
+s = [(0,0)]*(M+1)
+
+for i in range(1,M+1):
+    s[i] = tuple(map(int, input().split()))
+time = 0
+arrive = set()
+
+def bfs(i,j,m):
+    q = deque()
+    sr, sc = s[m]
+    q.append((i,j))
+    visited = [[0]*(N+1) for _ in range(N+1)]
+    visited[i][j] = 1
+    min_dist = 0
+    while q:
+        pr,pc = q.popleft()
+        min_dist+=1
+        if pr == sr and pc == sc:
+            return visited[pr][pc]-1
+        for dy, dx in [(-1, 0), (0, -1), (0, 1), (1, 0)]:
+            nr = pr + dy
+            nc = pc + dx
+            if 1<=nr<=N and 1<=nc<=N and arr[nr][nc] != -1 and visited[nr][nc] ==0:
+                visited[nr][nc] = visited[pr][pc]+1
+                q.append((nr,nc))
+
+
+    return 100
+
+
+def move(m):
+    pr, pc = p[m]  # 현재 위치
+    sr, sc = s[m]  # 목표 위치
+
+    # BFS로 sr, sc까지의 최단 거리 계산
+    q = deque()
+    q.append((sr, sc))
+    distance = [[-1] * (N + 1) for _ in range(N + 1)]
+    distance[sr][sc] = 0
+
+    while q:
+        cr, cc = q.popleft()
+        for dy, dx in [(-1, 0), (0, -1), (0, 1), (1, 0)]:
+            nr = cr + dy
+            nc = cc + dx
+            if 1 <= nr <= N and 1 <= nc <= N and arr[nr][nc] != -1 and distance[nr][nc] == -1:
+                distance[nr][nc] = distance[cr][cc] + 1
+                q.append((nr, nc))
+
+    # pr, pc에서 sr, sc까지의 최단 거리로 한 칸 이동
+    best_move = (pr, pc)
+    min_dist = float('inf')
+
+    for dy, dx in [(-1, 0), (0, -1), (0, 1), (1, 0)]:
+        nr = pr + dy
+        nc = pc + dx
+        if 1 <= nr <= N and 1 <= nc <= N and arr[nr][nc] != -1:
+            if distance[nr][nc] != -1 and distance[nr][nc] < min_dist:
+                min_dist = distance[nr][nc]
+                best_move = (nr, nc)
+
+    return best_move
+
+
+'''
+def move(m):
+    pr,pc = p[m]
+    sr,sc = s[m]
+    min_dist = 50
+    min_r = pr
+    min_c = pc
+    for dy,dx in [(-1,0),(0,-1),(0,1),(1,0)]:
+        nr = pr+dy
+        nc = pc+dx
+        if (nr,nc) == (sr,sc):
+            p[m] = (nr,nc)
+            return
+        if nr>N or nr<1 or nc>N or nc<1 or arr[nr][nc] != 0:
+            continue
+        if abs(nr-sr)+abs(nc-sc) < min_dist:
+            min_dist = abs(nr-sr)+abs(nc-sc)
+            min_r = nr
+            min_c = nc
+    p[m] = (min_r,min_c)
+'''
+
+def find_basecamp(m):
+    min_dist=100
+    min_r = 0
+    min_c = 0
+    for i in range(1,N+1):
+        for j in range(1,N+1):
+            if arr[i][j] != 1:
+                continue
+            bfs_dist = bfs(i,j,m)
+            if bfs_dist  < min_dist or (bfs_dist  == min_dist and i<min_r) or \
+                (bfs_dist  == min_dist and i==min_r and j <min_c ):
+                min_r = i
+                min_c = j
+                min_dist = bfs_dist
+
+    return (min_r,min_c)
+
+
+
+
+
+while True:
+    if len(arrive) == M:
+        break
+    time += 1
+    for m in range(1, M+1):
+
+        #3번 베이스먼트 찾기
+        if time<m:
+            continue
+        if p[m] == (0,0):
+            br,bc = find_basecamp(m)
+            arr[br][bc] = -1
+            p[m] = (br,bc)
+            continue
+        if m in arrive:
+            continue
+        p[m] = move(m)
+        pr, pc = p[m]
+        #1,2번 편의점으로 이동
+        if p[m] == s[m]:
+            arr[pr][pc] = -1  # 편의점 도착
+            arrive.add(m)
+            continue
+
+    '''
+    for m in range(1,M+1):
+        pr,pc = p[m]
+        if p[m] == s[m]:
+            arr[pr][pc] = -1 #편의점 도착
+            arrive.add(m)
+        if arr[pr][pc] == 1: #베이스먼트 도착
+            arr[pr][pc] = -1
+    '''
+print(time)
